0530d00149670fda242149f16145cd79555888f0
Jan Beilicke <jan@jotbe-fx.de>
2010-06-10 20:09:45 +0200

    Initial commit

---
+++ svn-check-rev.sh
@@ -0,0 +1,110 @@
+#!/bin/bash -e  
+#
+# Checks for available Subversion updates
+#
+# - Notifications are optionally send via Growl (growlnotify)
+# 
+# Take care to edit the variables as neccessary!
+#
+# Author: Jan Beilicke <j.beilicke@stroeer-interactive.de>
+# Created: 2010-06-10
+#
+# -------------------------------------------------------------------
+#
+# Usage: svn-check-rev.sh /path/to/svn/repository
+#
+
+### General ###
+PATH="/usr/bin:/opt/local/bin:/usr/local/bin:${PATH}"
+USEGROWL=false
+STICKY=false
+
+
+#### Do not edit anything below this line! ####
+NOTIFIER=`which growlnotify`
+SVN=`which svn`
+E_BADARGS=65
+
+usage() {
+  cat <<EOF
+
+Usage: `basename $0` [-h] [-n] [-s] /path/to/svn/working/copy/
+Parameters:
+-h          Display this help
+-n          Use Growl to notify about updates (requires 'growlnotify' in \$PATH)
+-s          Sticky growl notification (automatically enables Growl)
+
+EOF
+}
+
+while getopts "hnsd:" ARG; do
+  case "${ARG}" in
+    h )
+      usage
+      exit 1
+    ;;
+    n )
+      if [[ ! -e "$NOTIFIER" ]]; then
+        echo "Growl not found. Continuing without it ..."
+        USEGROWL=false
+      else
+        USEGROWL=true
+      fi
+    ;;
+    s )
+      if [[ ! -e "$NOTIFIER" ]]; then
+        echo "Growl not found. Continuing without it ..."
+        USEGROWL=false
+        STICKY=false
+      else
+        USEGROWL=true
+        STICKY=true
+      fi
+    ;;
+    ? ) 
+      usage
+      exit 1
+    ;;
+    * )
+      echo
+      echo "Unkown error while processing parameters."
+      exit 1
+  esac
+done
+
+if [[ ! -d "${!#}" ]]; then
+  echo
+  echo "Last argument isn't a directory or doesn't exist."
+  usage
+  exit 1
+fi
+
+SVNWCPATH="${!#}"
+
+[[ "$STICKY" = true ]] && SETSTICKY="-s"
+
+SVNURL=`$SVN info $SVNWCPATH | awk '/URL:/ {print $2}'`
+LOCALREV=`$SVN info $SVNWCPATH | awk '/Revision:/ {print $2}'`
+REMOTEREV=`$SVN info -rHEAD $SVNWCPATH | awk '/Revision:/ {print $2}'`
+
+echo
+echo "URL: $SVNURL"
+echo "Local revision: $LOCALREV"
+echo "Remote revision: $REMOTEREV"
+
+DIFFREV=$((REMOTEREV - LOCALREV))
+
+if [[ "$DIFFREV" -gt 0 ]]; then
+  if [[ "$USEGROWL" = true ]] && [[ -e "$NOTIFIER" ]]; then
+    "$NOTIFIER" "$SETSTICKY" -d "$SVNURL" -m "${SVNURL##*/}: Updates available! Remote rev. is $REMOTEREV (local rev. $LOCALREV)"
+  else
+    echo
+    echo "Updates available!"
+  fi
+else
+  echo
+  echo "No updates available."
+fi
+
+echo
+exit 0
